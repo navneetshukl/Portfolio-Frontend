3ec4bed984fe122a36a9dc232bb89659c8e9f102
navneetshukl <navneetshukla824@gmail.com>
2025-07-20 10:47:18 +0530

    some change

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "../header/header";
+import Header from "../header/Header";
 
 // Import icons from react-icons/fa for GitHub and LinkedIn
 import { FaGithub, FaLinkedin } from "react-icons/fa";
